223fd2bd60c1fd232c6623118b11757b0a6727e9
anqqa <anttiq@gmail.com>
2010-05-25 20:55:23 +0300

    Base config file

---
+++ config/anqh.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Base Anqh config file
+ *
+ * @package    Anqh
+ * @author     Antti Qvickström
+ * @copyright  (c) 2009-2010 Antti Qvickström
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT license
+ */
+
+return array(
+	'version' => '0.2',
+	'status'  => 'alpha',
+);
