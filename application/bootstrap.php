a9ddd0ffb3188badd73c12403bf0e128117b4dc6
anqqa <anttiq@gmail.com>
2010-05-25 20:24:24 +0300

    index, install and bootstrap

---
+++ application/bootstrap.php
@@ -0,0 +1,133 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+//-- Environment setup --------------------------------------------------------
+
+/**
+ * Set the default time zone.
+ *
+ * @see  http://kohanaframework.org/guide/using.configuration
+ * @see  http://php.net/timezones
+ */
+date_default_timezone_set('Europe/Helsinki');
+
+/**
+ * Set the default locale.
+ *
+ * @see  http://kohanaframework.org/guide/using.configuration
+ * @see  http://php.net/setlocale
+ */
+setlocale(LC_ALL, 'en_US.utf-8');
+
+/**
+ * Enable the Kohana auto-loader.
+ *
+ * @see  http://kohanaframework.org/guide/using.autoloading
+ * @see  http://php.net/spl_autoload_register
+ */
+spl_autoload_register(array('Kohana', 'auto_load'));
+
+/**
+ * Set the production status by the domain.
+ */
+define('IN_PRODUCTION', !isset($_ENV['DEVELOPMENT']));
+
+/**
+ * Enable the Kohana auto-loader for unserialization.
+ *
+ * @see  http://php.net/spl_autoload_call
+ * @see  http://php.net/manual/var.configuration.php#unserialize-callback-func
+ */
+ini_set('unserialize_callback_func', 'spl_autoload_call');
+
+//-- Configuration and initialization -----------------------------------------
+
+/**
+ * Initialize Kohana, setting the default options.
+ *
+ * The following options are available:
+ *
+ * - string   base_url    path, and optionally domain, of your application   NULL
+ * - string   index_file  name of your index file, usually "index.php"       index.php
+ * - string   charset     internal character set used for input and output   utf-8
+ * - string   cache_dir   set the internal cache directory                   APPPATH/cache
+ * - boolean  errors      enable or disable error handling                   TRUE
+ * - boolean  profile     enable or disable internal profiling               TRUE
+ * - boolean  caching     enable or disable internal caching                 FALSE
+ */
+Kohana::init(array(
+	'base_url'   => '/',
+	'index_file' => false,
+	'profile'    => !IN_PRODUCTION,
+));
+
+/**
+ * Attach the file write to logging. Multiple writers are supported.
+ */
+Kohana::$log->attach(new Kohana_Log_File(APPPATH.'logs'));
+
+/**
+ * Attach a file reader to config. Multiple readers are supported.
+ */
+Kohana::$config->attach(new Kohana_Config_File);
+
+/**
+ * Enable modules. Modules are referenced by a relative or absolute path.
+ */
+Kohana::modules(array(
+	// 'auth'       => MODPATH.'auth',       // Basic authentication
+	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
+	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
+	// 'database'   => MODPATH.'database',   // Database access
+	// 'image'      => MODPATH.'image',      // Image manipulation
+	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+	// 'pagination' => MODPATH.'pagination', // Paging of results
+	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+	));
+
+/**
+ * Set the routes. Each route must have a minimum of a name, a URI and a set of
+ * defaults for the URI.
+ */
+Route::set('default', '(<controller>(/<action>(/<id>)))')
+	->defaults(array(
+		'controller' => 'welcome',
+		'action'     => 'index',
+	));
+
+/**
+ * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
+ * If no source is specified, the URI will be automatically detected.
+ */
+$request = Request::instance();
+try {
+
+	// Attempt to execute the response
+	$request->execute();
+
+} catch (Exception $e) {
+
+	// Throw errors in development environment
+	if (!IN_PRODUCTION) {
+		throw $e;
+	}
+
+	// Log errors
+	Kohana::$log->add(Kohana::ERROR, Kohana::exception_text($e));
+
+}
+
+/**
+ * Add statistics to the response.
+ */
+if ($request->response) {
+	$total = array(
+		'{memory_usage}'   => number_format((memory_get_peak_usage() - KOHANA_START_MEMORY) / 1024, 2) . 'KB',
+		'{execution_time}' => number_format(microtime(true) - KOHANA_START_TIME, 5) . ' seconds'
+	);
+	$request->response = strtr((string)$request->response, $total);
+}
+
+/**
+ * Display the request response.
+ */
+echo $request->send_headers()->response;
