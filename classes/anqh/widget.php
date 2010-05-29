92edb76a93e95c3b6f12331eef49dbb71f47ce19
anqqa <anttiq@gmail.com>
2010-05-29 13:04:44 +0300

    Widget class

---
+++ classes/anqh/widget.php
@@ -0,0 +1,104 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * Widget helper for adding content to views.
+ *
+ * Based on widget helper in YurikoCMS by Lorenzo Pisani,
+ * see official site for more info: http://github.com/Zeelot/yuriko_cms/tree
+ *
+ * @package    Anqh
+ * @author     Antti Qvickström
+ * @copyright  (c) 2010 Antti Qvickström
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT license
+ */
+class Anqh_Widget {
+
+	/**
+	 * Weights
+	 *
+	 * @var  array
+	 */
+	protected static $weights = array();
+
+	/**
+	 * Widgets
+	 *
+	 * @var  array
+	 */
+	protected static $widgets = array();
+
+
+	/**
+	 * Add new content for a region.
+	 * Optionally specifying a weight to determine its output placement.
+	 * Lower weight is higher: $weight = -100 gets output before 100.
+	 *
+	 * @param  string  $name
+	 * @param  string  $content
+	 * @param  int     $weight
+	 */
+	public static function add($name, $content, $weight = 0) {
+	  while (in_array($weight, self::$weights)) {
+	    $weight++;
+	  }
+	  self::$weights[] = $weight;
+	  self::$widgets[$name][$weight] = $content;
+	}
+
+
+	/**
+	 * Clear content from region.
+	 *
+	 * @param  string  $name
+	 */
+	public static function clear($name) {
+		unset(self::$widgets[$name]);
+	}
+
+
+	/**
+	 * Get the contents of a widget region, output is "first in first out" unless
+	 * a weight was specified during set().
+	 *
+	 * @param   string  $name
+	 * @param   string  $glue
+	 * @return  string
+	 */
+	public static function get($name, $glue = '') {
+	  if (isset(self::$widgets[$name])) {
+	    ksort(self::$widgets[$name]);
+	    return implode($glue, self::$widgets[$name]);
+	  } else {
+	    return '';
+	  }
+	}
+
+
+	/**
+	 * Checks to see if there is content within a widget region.
+	 *
+	 * @param   string  $name
+	 * @return  boolean
+	 */
+	public static function is_set($name) {
+	  return !empty(self::$widgets[$name]);
+	}
+
+
+	/**
+	 * Override content to region
+	 * Optionally specifying a weight to determine its output placement.
+	 * Lower weight is higher: $weight = -100 gets output before 100.
+	 *
+	 * @param  string  $name
+	 * @param  string  $content
+	 * @param  int     $weight
+	 */
+	public static function set($name, $content, $weight = 0) {
+		$weights = self::$widgets[$name];
+		self::$weights = array_diff(self::$weights, $weights);
+		self::$widgets[$name] = array();
+
+		self::add($name, $content, $weight);
+	}
+
+}
