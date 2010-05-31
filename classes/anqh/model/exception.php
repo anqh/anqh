9f0e064011b29bfd0d59733ba7991043a21d4fc7
anqqa <anttiq@gmail.com>
2010-05-31 21:55:02 +0300

    Model_Exception not so stupid anymore, eh

--- classes/anqh/model/exception.php
+++ classes/anqh/model/exception.php
@@ -9,6 +9,33 @@
  */
 class Anqh_Model_Exception extends Kohana_Exception {
 
-	const WARNING = 'WARNING';
+	const NOT_FOUND   = 0;
+	const PERMISSION  = 1;
+	const ERROR_LEVEL = 'WARNING';
+
+
+	/**
+	 * Model not found or no access
+	 *
+	 * @param  Jelly_Model $model
+	 * @param  integer     $id
+	 * @param  integer     $code
+	 * @param  string      $permission
+	 * @param  integer     $code
+	 */
+	public function __construct(Jelly_Model $model, $id = 0, $code = self::NOT_FOUND, $permission = null) {
+		$values = array(
+			':id'    => $id,
+			':model' => Jelly::model_name($model),
+		);
+
+		switch ($code) {
+			case self::NOT_FOUND: $message = 'Model not found: :id @ :model'; break;
+			case self::PERMISSION: $message = "Permission ':permission' denied: :id @ :model"; $values[':permission'] = $permission; break;
+			default: $message = 'Model Exception: :id @ :model'; break;
+		}
+
+		parent::__construct($message, $values, $code);
+	}
 
 }
