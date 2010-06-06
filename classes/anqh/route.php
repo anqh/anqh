e2026ddadc99bce6c28840287108fc7436e88534
anqqa <anttiq@gmail.com>
2010-06-06 21:04:21 +0300

    Simplify model()

--- classes/anqh/route.php
+++ classes/anqh/route.php
@@ -10,26 +10,20 @@
 class Anqh_Route extends Kohana_Route {
 
 	/**
-	 * Return model specific route, if $params is null, use $action as $params
+	 * Return model specific route
 	 *
 	 * @param   Jelly_Model  $model
 	 * @param   string       $action
 	 * @param   string       $params
+	 * @param   string       $route   Defaults to model name
 	 * @return  string
 	 */
-	public static function model(Jelly_Model $model, $action = null, $params = null) {
-		$route  = Jelly::model_name($model);
-		$id     = URL::title($model->id() . ' ' . $model->name());
-		$uri    = array(
-			'id'     => $id,
-			'action' => $action && !is_null($params) ? $action : null,
-			'params' => is_null($params) ? $action : ($params ? $params : null)
-		);
-		try {
-			return Route::get($route)->uri($uri);
-		} catch (Kohana_Exception $e) {
-			return $route . '/' . $id . ($uri['action'] ? '/' . $uri['action'] : '') . ($uri['params'] ? '/' . $uri['params'] : '');
-		}
+	public static function model(Jelly_Model $model, $action = null, $params = null, $route = null) {
+		return Route::get($route ? $route : Jelly::model_name($model))->uri(array(
+			'id'     => URL::title($model->id() . ' ' . $model->name()),
+			'action' => $action,
+			'params' => $params,
+		));
 	}
 
 }
