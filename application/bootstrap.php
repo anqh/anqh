7d642de4316ec3bdbfb7a9e68eae00c1925ed0d5
anqqa <anttiq@gmail.com>
2010-06-19 19:51:05 +0300

    Move Anqh specific routes to Anqh init

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -80,6 +80,7 @@ Kohana::modules(array(
 	'events'     => MODPATH . 'events',     // Anqh event calendar
 	'forum'      => MODPATH . 'forum',      // Anqh forums
 	'venues'     => MODPATH . 'venues',     // Anqh venues
+	'blog'       => MODPATH . 'blog',       // Anqh blogs
 
 	'database'   => MODPATH . 'database',   // Database access
 	'jelly'      => MODPATH . 'jelly',      // Jelly ORM
@@ -100,68 +101,11 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('404', '<file>.<ext>', array('ext' => 'ico|png|jpg|gif|txt|avi|flv|sql|js|css'))
-	->defaults(array(
-		'controller' => 'static',
-		'action'     => '404'
-	));
-Route::set('sign', 'sign/<action>', array('action' => 'up|in|out'))
-	->defaults(array(
-		'controller' => 'sign',
-		'action'     => 'up'
-	));
-Route::set('shouts', 'shouts(/<action>)', array('action' => 'index|shout'))
-	->defaults(array(
-		'controller' => 'shouts',
-		'action'     => 'index'
-	));
-Route::set('roles', 'roles')
-	->defaults(array(
-		'controller' => 'roles',
-		'action'     => 'index',
-	));
-Route::set('role', 'role(/<id>(/<action>))', array('action' => 'delete|edit'))
-	->defaults(array(
-		'controller' => 'roles',
-		'action'     => 'edit',
-	));
-Route::set('tag_group_add', 'tags/addgroup')
-	->defaults(array(
-		'controller' => 'tags',
-		'action'     => 'addgroup',
-	));
-Route::set('tag_group', 'tags/<id>(/<action>)', array('action' => 'group|add|deletegroup|editgroup'))
-	->defaults(array(
-		'controller' => 'tags',
-		'action'     => 'group',
-	));
-Route::set('tag', 'tag/<id>(/<action>)', array('action' => 'tag|edit|delete'))
-	->defaults(array(
-		'controller' => 'tags',
-		'action'     => 'tag',
-	));
-Route::set('tags', 'tags')
-	->defaults(array(
-		'controller' => 'tags',
-		'action'     => 'index',
-	));
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'index',
 		'action'     => 'index',
 	));
-/*
-Route::set('index', '')
-	->defaults(array(
-		'controller' => 'index',
-		'action'     => 'index',
-	));
-Route::set('catch_all', '(<path>)', array('path' => '.+'))
-	->defaults(array(
-		'controller' => 'error',
-		'action' => '404'
-	));
-*/
 
 /**
  * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
