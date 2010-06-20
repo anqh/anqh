fd9482556767df362ac6d571d4424a1d1a3584cd
anqqa <anttiq@gmail.com>
2010-06-20 14:43:31 +0300

    Image comments

--- classes/anqh/model/image.php
+++ classes/anqh/model/image.php
@@ -37,6 +37,29 @@ class Anqh_Model_Image extends Jelly_Model implements Permission_Interface {
 			)),
 			'description' => new Field_String,
 			'format'      => new Field_String,
+			'created' => new Field_Timestamp(array(
+				'auto_now_create' => true,
+			)),
+			'num_views' => new Field_Integer(array(
+				'column' => 'views',
+			)),
+			'num_comments' => new Field_Integer(array(
+				'column' => 'comments',
+			)),
+			'num_votes' => new Field_Integer(array(
+				'column' => 'votes',
+			)),
+			'score' => new Field_Integer,
+
+			'original_size'   => new Field_Integer,
+			'original_width'  => new Field_Integer,
+			'original_height' => new Field_Integer,
+			'width'           => new Field_Integer,
+			'height'          => new Field_Integer,
+			'thumb_width'     => new Field_Integer,
+			'thumb_height'    => new Field_Integer,
+
+			'legacy_filename' => new Field_String,
 
 			'author'   => new Field_BelongsTo(array(
 				'column'  => 'author_id',
@@ -58,8 +81,6 @@ class Anqh_Model_Image extends Jelly_Model implements Permission_Interface {
 	 * @return  boolean
 	 */
 	public function has_permission($permission, $user) {
-		$status = false;
-
 		switch ($permission) {
 			case self::PERMISSION_CREATE:
 			case self::PERMISSION_DELETE:
@@ -67,7 +88,7 @@ class Anqh_Model_Image extends Jelly_Model implements Permission_Interface {
 			case self::PERMISSION_UPDATE:
 		}
 
-		return $status;
+		return false;
 	}
 
 
---
+++ classes/anqh/model/image/comment.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Image Comment model
+ *
+ * @package    Anqh
+ * @author     Antti Qvickström
+ * @copyright  (c) 2010 Antti Qvickström
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT license
+ */
+class Anqh_Model_Image_Comment extends Model_Comment implements Permission_Interface {
+
+	/**
+	 * Create new model
+	 *
+	 * @param  Jelly_Meta  $meta
+	 */
+	public static function initialize(Jelly_Meta $meta) {
+		$meta = parent::initialize($meta);
+
+		$meta->fields(array(
+			'image' => new Field_BelongsTo
+		));
+	}
+
+}
