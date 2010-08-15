6506b138f0f47d94e2eb8a96f08265a40eacb982
anqqa <anttiq@gmail.com>
2010-08-15 14:45:09 +0300

    Fix flyers when using uploaded flyers

--- views/events/flyers.php
+++ views/events/flyers.php
@@ -10,8 +10,8 @@
 ?>
 
 <?php if ($event->flyer_front->id || $event->flyer_back->id): ?>
-	<?php echo $event->flyer_front->id ? HTML::img($event->flyer_front, 'normal', array('title' => __('Flyer, front'), 'width' => '100%')) : '' ?>
-	<?php echo $event->flyer_back->id  ? HTML::img($event->flyer_back,  'normal', array('title' => __('Flyer, back'),  'width' => '100%')) : '' ?>
+	<?php echo $event->flyer_front->id ? HTML::image($event->flyer_front->get_url('normal'), array('title' => __('Flyer, front'), 'width' => '100%')) : '' ?>
+	<?php echo $event->flyer_back->id  ? HTML::image($event->flyer_back->get_url('normal'),  array('title' => __('Flyer, back'),  'width' => '100%')) : '' ?>
 <?php elseif ($event->flyer_front_url || $event->flyer_back_url): ?>
 	<?php echo $event->flyer_front_url ? HTML::image($event->flyer_front_url, array('alt' => __('Flyer, front'), 'title' => __('Flyer, front'), 'width' => '100%')) : '' ?>
 	<?php echo $event->flyer_back_url  ? HTML::image($event->flyer_back_url,  array('alt' => __('Flyer, back'),  'title' => __('Flyer, back'),  'width' => '100%')) : '' ?>
