52e42c7cd4fe78abf1687c3dd3b8986b4dd19a69
anqqa <anttiq@gmail.com>
2010-09-03 22:25:13 +0300

    Topic list UI fix

--- views/forum/topics.php
+++ views/forum/topics.php
@@ -16,7 +16,7 @@
 	<article>
 		<header>
 			<small class="ago"><?php echo HTML::time(Date::short_span($topic->last_posted, true, true), $topic->last_posted) ?></small>
-			<?php echo HTML::anchor(Route::model($topic, '?page=last#last'), HTML::chars($topic->name)) ?>
+			<h4><?php echo HTML::anchor(Route::model($topic, '?page=last#last'), HTML::chars($topic->name)) ?></h4>
 		</header>
 		<?php echo HTML::user($topic->last_post->author, $topic->last_poster) ?>
 		<?php echo HTML::icon_value(array(':replies' => $topic->post_count - 1), ':replies reply', ':replies replies', 'posts' . ($topic->post_count > 49 ? ' hot' : '')) ?>
