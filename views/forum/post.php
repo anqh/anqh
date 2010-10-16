20ae7b20abc8350403458d93fdcbcef4828cd57f
anqqa <anttiq@gmail.com>
2010-10-16 16:55:32 +0300

    Layout fixing

--- views/forum/post.php
+++ views/forum/post.php
@@ -16,12 +16,27 @@ $owners = ($topic->author->id && $post->author->id == $topic->author->id);
 ?>
 
 	<article id="post-<?php echo $post->id ?>" class="post <?php echo ($owners ? 'owner ' : ''), ($my ? 'my ' : ''), Text::alternate('', 'alt') ?>">
-		<header<?php echo $post->id == $topic->last_post->id ? ' id="last"' : '' ?>>
+		<header<?php echo $post->id == $topic->last_post->id ? ' id="last"' : '' ?> class="grid2 first">
 
 			<?php echo HTML::avatar($post->author->avatar, $post->author->username) ?>
 
 			<?php echo HTML::user($post->author, $post->author_name) ?>
+		</header>
+
+		<section class="post-content grid6">
 			<small class="ago"><?php echo HTML::time(Date::short_span($post->created, true, true), $post->created) ?></small>
+
+			<?php if ($post->parent->id) echo __('Replying to :parent', array(
+				':parent' => HTML::anchor(
+					Route::get('forum_post')->uri(array('topic_id' => Route::model_id($topic), 'id' => $post->parent->id)) . '#post-' . $post->parent->id,
+					HTML::chars($post->parent->topic->name)
+				))) ?>
+
+<?php echo BB::factory($post->post)->render() ?>
+
+		</section>
+
+		<div class="grid2 first">
 			<span class="actions alt">
 				<?php if (Permission::has($post, Model_Forum_Post::PERMISSION_UPDATE, $user)) echo HTML::anchor(
 						Route::get('forum_post')->uri(array(
@@ -47,32 +62,11 @@ $owners = ($topic->author->id && $post->author->id == $topic->author->id);
 						__('Quote'),
 						array('class' => 'action post-quote small')) ?>
 			</span>
+		</div>
 
-			<?php if ($post->modifies > 0): ?>
-			<br />
-			<?php echo __('Edited :ago', array(
-				':ago' => HTML::time(Date::fuzzy_span($post->modified), $post->modified)
-			)) ?>
-			<?php endif;
-			if ($post->parent->id): ?>
-			<br />
-			<?php echo __('Reply to :parent', array(
-				':parent' => HTML::anchor(
-					Route::get('forum_post')->uri(array(
-						'topic_id' => Route::model_id($topic),
-						'id'       => $post->parent->id,
-					)) . '#post-' . $post->parent->id,
-					HTML::chars($post->parent->topic->name)))) ?>
-			<?php endif; ?>
-		</header>
+		<footer class="grid6">
+			<?php if ($post->modifies > 0) echo	__('Edited :ago', array(':ago' => HTML::time(Date::fuzzy_span($post->modified), $post->modified))); ?>
 
-		<section class="post-content">
-
-<?php echo BB::factory($post->post)->render() ?>
-
-		</section>
-
-		<footer>
 			<?php echo $post->author->signature ? BB::factory("\n--\n" . $post->author->signature)->render() : '' ?>
 
 		</footer>
