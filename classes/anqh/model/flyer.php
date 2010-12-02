be27373c6695d2a9d121af646c4b4bf79964a2cb
anqqa <anttiq@gmail.com>
2010-12-02 20:45:17 +0200

    Month browsing

--- classes/anqh/model/flyer.php
+++ classes/anqh/model/flyer.php
@@ -49,6 +49,27 @@ class Anqh_Model_Flyer extends Jelly_Model implements Permission_Interface {
 
 
 	/**
+	 * Find flyers by year and month
+	 *
+	 * @static
+	 * @param   integer  $year
+	 * @param   integer  $month
+	 * @return  Jelly_Collection
+	 */
+	public static function find_by_month($year, $month) {
+		$start = mktime(0, 0, 0, $month, 1, $year);
+		$end   = strtotime('+1 month', $start);
+		return Jelly::select('flyer')
+			->join('event')
+			->on('flyer.event:foreign_key', '=', 'events.id')
+			->where('stamp_begin', 'BETWEEN', array($start, $end))
+			->order_by('stamp_begin', 'DESC')
+			->order_by('events.id', 'DESC')
+			->execute();
+	}
+
+
+	/**
 	 * Find latest flyers
 	 *
 	 * @static
@@ -60,6 +81,41 @@ class Anqh_Model_Flyer extends Jelly_Model implements Permission_Interface {
 			->limit((int)$limit)
 			->order_by('image_id', 'DESC')
 			->execute();
+	}
+
+
+	/**
+	 * Get months with flyers.
+	 * Returns array of years => months => count
+	 *
+	 * @static
+	 * @return  array
+	 */
+	public static function find_months() {
+		$months = array();
+
+		// Build counts
+		$flyers = DB::query(
+			Database::SELECT,
+			"
+SELECT TO_CHAR(TO_TIMESTAMP(events.stamp_begin), 'YYYY MM') AS month, COUNT(image_id) AS flyers
+FROM flyers INNER JOIN events ON (flyers.event_id = events.id)
+GROUP BY 1
+"
+		)->execute();
+
+		foreach ($flyers as $flyer) {
+			list($year, $month) = explode(' ', $flyer['month']);
+			$months[(int)$year][(int)$month] = $flyer['flyers'];
+		}
+
+		// Sort years
+		krsort($months);
+		foreach ($months as &$year) {
+			krsort($year);
+		}
+
+		return $months;
 	}
 
 
