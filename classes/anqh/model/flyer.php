0133973451262bb53cb4a0505152580046f59e08
anqqa <anttiq@gmail.com>
2010-12-02 19:27:15 +0200

    Comments

--- classes/anqh/model/flyer.php
+++ classes/anqh/model/flyer.php
@@ -10,17 +10,41 @@
 class Anqh_Model_Flyer extends Jelly_Model implements Permission_Interface {
 
 	/**
+	 * Permission to post comments
+	 */
+	const PERMISSION_COMMENT = 'comment';
+
+	/**
+	 * Permission to read comments
+	 */
+	const PERMISSION_COMMENTS = 'comments';
+
+
+	/**
 	 * Create new model
 	 *
 	 * @param  Jelly_Meta  $meta
 	 */
 	public static function initialize(Jelly_Meta $meta) {
-		$meta->table('events_flyers');
 		$meta->fields(array(
 			'id'      => new Field_Primary,
 			'image'   => new Field_BelongsTo,
 			'event'   => new Field_BelongsTo,
 		));
+	}
+
+
+	/**
+	 * Find flyer by image id
+	 *
+	 * @param   integer  $image_id
+	 * @return  Model_Flyer
+	 */
+	public static function find_by_image($image_id) {
+		return Jelly::select('flyer')
+			->where('image_id', '=', (int)$image_id)
+			->limit(1)
+			->execute();
 	}
 
 
@@ -48,10 +72,15 @@ class Anqh_Model_Flyer extends Jelly_Model implements Permission_Interface {
 	 */
 	public function has_permission($permission, $user) {
 		switch ($permission) {
-			case self::PERMISSION_CREATE:
 			case self::PERMISSION_DELETE:
-			case self::PERMISSION_READ:
 			case self::PERMISSION_UPDATE:
+				return $user && $user->has_role(array('admin', 'photo moderator'));
+
+			case self::PERMISSION_COMMENT:
+			case self::PERMISSION_COMMENTS:
+			case self::PERMISSION_CREATE:
+			case self::PERMISSION_READ:
+		    return (bool)$user;
 		}
 
 		return false;
