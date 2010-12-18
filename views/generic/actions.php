badbf82a248381ce8da228835803734cdaef0835
anqqa <anttiq@gmail.com>
2010-12-19 01:35:39 +0200

    Add space

--- views/generic/actions.php
+++ views/generic/actions.php
@@ -18,7 +18,7 @@
 				$attributes = $action;
 				unset($attributes['link'], $attributes['text']);
 				$attributes['class'] = isset($attributes['class']) ? 'action ' . $attributes['class'] : 'action';
-				echo HTML::anchor($action['link'], $action['text'], $attributes);
+				echo HTML::anchor($action['link'], $action['text'], $attributes) . ' ';
 
 			else:
 
