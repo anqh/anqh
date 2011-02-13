ae1210818e7184397dc628cf66d7f26d2f0202e7
anqqa <anttiq@gmail.com>
2011-02-13 20:01:30 +0200

    Random flyer and flyer editing improvements

--- www/js/anqh.js
+++ www/js/anqh.js
@@ -328,6 +328,87 @@ $.fn.autocompleteCity = function(options) {
 		});
 };
 
+$.fn.autocompleteEvent = function(options) {
+	var field = $(this);
+	var cache = {};
+	var lastXhr;
+
+	var defaults = {
+		'eventId':   'event_id',
+		'limit':     25,
+		'minLength': 3,
+		'action':    'form',
+		'search':    'name',
+		'field':     'id:name:city:stamp_begin:url',
+		'order':     'stamp_begin.desc'
+	};
+	options = $.extend(defaults, options || {});
+
+	$(this)
+		.autocomplete({
+			minLength: options.minLength,
+
+			source: function(request, response) {
+				if (request.term in cache) {
+					response(cache[request.term]);
+					return;
+				}
+
+				lastXhr = $.getJSON(
+					'/api/v1/events/search',
+					{
+						'q':      request.term,
+						'limit':  25,
+						'filter': options.filter,
+						'search': options.search,
+						'field':  options.field,
+						'order':  options.order
+					},
+					function(data, status, xhr) {
+						cache[request.term] = $.map(data.events, function(item) {
+							return {
+								'label': item.name,
+								'stamp': item.stamp_begin,
+								'city':  item.city,
+								'value': item.name,
+								'id':    item.id,
+								'url':   item.url
+							}
+						});
+
+						if (xhr === lastXhr) {
+							response(cache[request.term]);
+						}
+					}
+				);
+			},
+
+			select: function(event, ui) {
+				switch (options.action) {
+
+					// Fill form
+					case 'form':
+						$('input[name=' + options.eventId + ']') && $('input[name=' + options.eventId + ']').val(ui.item.id);
+						field.val(ui.item.value);
+						break;
+
+					// Navigate URL
+					case 'redirect':
+						window.location = ui.item.url;
+						break;
+
+				}
+			}
+
+		})
+		.data('autocomplete')._renderItem = function(ul, item) {
+			return $("<li></li>")
+				.data('item.autocomplete', item)
+				.append('<a>' + $.datepicker.formatDate('dd.mm.yy', new Date(item.stamp * 1000)) + ' ' + item.label + ', ' + item.city + '</a>')
+				.appendTo(ul);
+		};
+};
+
 
 // User autocomplete
 $.fn.autocompleteUser = function(options) {
