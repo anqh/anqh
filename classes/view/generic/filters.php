f3b2780e3974de80907e32c9dcae6355a597dfad
anqqa <anttiq@gmail.com>
2012-11-13 18:45:48 +0200

    Support for non-js filtering

--- classes/view/generic/filters.php
+++ classes/view/generic/filters.php
@@ -9,20 +9,62 @@
  */
 class View_Generic_Filters extends View_Section {
 
+	/** Filter with JavaScript */
+	const TYPE_JAVASCRIPT = 'javascript';
+
+	/** Filter with request, filter in query param */
+	const TYPE_QUERY = 'query';
+
+	/** Filter with request, filter in url */
+	const TYPE_URL = 'url';
+
+	/**
+	 * @var  string  Base URL
+	 */
+	public $base_url;
+
+	/**
+	 * @var  boolean  Clear query string
+	 */
+	public $clear_query = false;
+
 	/**
 	 * @var  array  Filters
 	 */
 	public $filters = array();
 
+	/**
+	 * @var  boolean  Multiple filters per query
+	 */
+	public $multiple_query = false;
+
+	/**
+	 * @var  string  Query string parameter
+	 */
+	public $parameter = 'page';
+
+	/**
+	 * @var  string  selected item
+	 */
+	public $selected;
+
+	/**
+	 * @var  string  Filter type
+	 */
+	public $type;
+
 
 	/**
 	 * Create new view.
 	 */
-	public function __construct() {
+	public function __construct(array $filters = null, $selected = null) {
 		parent::__construct();
 
 		$this->id    = 'filters';
 		$this->class = 'filters';
+
+		$this->filters  = $filters;
+		$this->selected = $selected;
 	}
 
 
@@ -38,24 +80,73 @@ class View_Generic_Filters extends View_Section {
 
 		ob_start();
 
-		foreach ($this->filters as $type => $filter) {
+		if ($this->type == self::TYPE_JAVASCRIPT):
+
+			// Filter with javascript
+			foreach ($this->filters as $type => $filter):
+
 ?>
 
 <div class="btn-toolbar filters">
 	<div class="btn-group" data-toggle="buttons-checkbox">
-		<a data-filter="all" class="btn btn-mini btn-inverse active"><?php echo __('All') ?></a>
+		<a data-filter="all" class="btn btn-mini btn-inverse active"><?= __('All') ?></a>
 	</div>
 
 	<div class="btn-group" data-toggle="buttons-checkbox">
 
 		<?php foreach ($filter['filters'] as $key => $name) { ?>
-		<a data-filter="<?php echo $type . '-' . $key ?>" class="btn btn-mini btn-inverse"><?php echo HTML::chars($name) ?></a>
+		<a data-filter="<?= $type . '-' . $key ?>" class="btn btn-mini btn-inverse"><?= HTML::chars($name) ?></a>
 		<?php } ?>
 
 	</div>
 </div>
 
-<?php } ?>
+<?php
+
+			echo $this->javascript();
+
+			endforeach;
+
+		else:
+
+			// Filter with query
+			foreach ($this->filters as $type => $filter):
+
+?>
+
+<div class="btn-toolbar filters">
+	<div class="btn-group" data-toggle="buttons-checkbox">
+		<a href="<?= self::url(false) ?>" class="btn btn-mini btn-inverse <?= $this->selected ? '' : 'active' ?>"><?= __('All') ?></a>
+	</div>
+
+	<div class="btn-group" data-toggle="buttons-checkbox">
+
+		<?php foreach ($filter['filters'] as $key => $name): ?>
+		<a href="<?= self::url($name) ?>" class="btn btn-mini btn-inverse <?= $this->selected == $name ? 'active' : '' ?>"><?= HTML::chars($name) ?></a>
+		<?php endforeach; ?>
+
+	</div>
+</div>
+
+<?php
+
+			endforeach;
+
+		endif;
+
+		return ob_get_clean();
+	}
+
+
+	/**
+	 * JavaScripts for live filters.
+	 *
+	 * @return  string
+	 */
+	public function javascript() {
+		ob_start();
+
+?>
 
 <script>
 (function() {
@@ -113,6 +204,48 @@ class View_Generic_Filters extends View_Section {
 <?php
 
 		return ob_get_clean();
+	}
+
+
+	/**
+	 * Generates the full URL for a certain page.
+	 *
+	 * @param   string|boolean  $filter
+	 * @return  string
+	 */
+	public function url($filter) {
+		if ($this->type === self::TYPE_URL) {
+			return URL::site($this->base_url . '/' . $filter);
+		}
+
+		$query = $_GET;
+		if ($filter === false) {
+
+			// Remove filters
+			unset($query[$this->parameter]);
+
+		} elseif ($this->multiple_query) {
+
+			// Add filter
+			$filters   = explode(',', Arr::get($query, $this->parameter));
+			$filters[] = $filter;
+			$query[$this->parameter] = implode(',', $filters);
+
+		} else {
+
+			// Replace filter
+			$query[$this->parameter] = $filter;
+
+		}
+
+		// Clear other parameters?
+		if ($this->clear_query) {
+			$query = Arr::intersect($query, array($this->parameter));
+		}
+
+		list($uri) = $this->base_url ? array($this->base_url) : explode('?', Request::current()->current_uri());
+
+		return URL::site($uri) . URL::query($query);
 	}
 
 }
