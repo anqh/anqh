1e9b77772b151987b3a88510e14ca958f65e86e8
anqqa <anttiq@gmail.com>
2012-12-22 16:19:46 +0200

    Fix time span with day name

--- classes/anqh/date.php
+++ classes/anqh/date.php
@@ -235,7 +235,7 @@ class Anqh_Date extends Kohana_Date {
 		} else if ($offset < Date::DAY) {
 			$span = __(':hour h', array(':hour' => sprintf($wrap, floor($offset / Date::HOUR))));
 		} else if ($offset < Date::WEEK) {
-			$span = date('l', $timestamp); //__(':day d', array(':day' => sprintf($wrap, floor($offset / Date::DAY))));
+			return date('l', $timestamp); //__(':day d', array(':day' => sprintf($wrap, floor($offset / Date::DAY))));
 		} else if ($offset < Date::MONTH) {
 			$span = __(':week wk', array(':week' => sprintf($wrap, floor($offset / Date::WEEK))));
 		} else if ($offset < (Date::YEAR)) {
