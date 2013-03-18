25da24fd8f87899df326dccc91c9efa1c399867a
anqqa <anttiq@gmail.com>
2013-03-18 19:25:17 +0200

    Dialog styling

--- www/js/anqh.js
+++ www/js/anqh.js
@@ -134,19 +134,21 @@ $.fn.skinswitcher = function() {
 // Open ajax dialog
 $.fn.dialogify = function() {
 	var href = this.attr('href') || this.attr('data-href');
-	if (!href) { return false; }
+	if (!href) {
+		return false;
+	}
 
-	var title = this.attr('data-dialog-title');
-	var width = this.attr('data-dialog-width') || 300;
-	var height = this.attr('data-dialog-height') || 'auto';
+	var title  = this.attr('data-dialog-title'),
+	    width  = this.attr('data-dialog-width') || 300,
+	    height = this.attr('data-dialog-height') || 'auto';
 	$('<div style="display:none"></div>')
 		.appendTo('body')
 		.dialog({
-			modal: true,
-			title: title,
-			width: width,
-			height: height,
-			closeText: '✕',
+			modal:     true,
+			title:     title,
+			width:     width,
+			height:    height,
+			closeText: '☓',
 			open: function() {
 				$(this).load(href);
 			},
