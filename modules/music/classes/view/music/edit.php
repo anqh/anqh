bc221b2430703b12d6a5d2a8b2404f68b79a472a
anqqa <anttiq@gmail.com>
2014-03-21 15:15:47 +0200

    Edit

--- modules/music/classes/view/music/edit.php
+++ modules/music/classes/view/music/edit.php
@@ -1,10 +1,10 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 /**
- * View_Music_Edit
+ * Music Edit Form.
  *
  * @package    Music
  * @author     Antti Qvickström
- * @copyright  (c) 2012 Antti Qvickström
+ * @copyright  (c) 2012-2014 Antti Qvickström
  * @license    http://www.opensource.org/licenses/mit-license.php MIT license
  */
 class View_Music_Edit extends View_Article {
@@ -45,32 +45,45 @@ class View_Music_Edit extends View_Article {
 	public function content() {
 		ob_start();
 
-		echo Form::open(null, array('id' => 'form-music', 'class' => 'row-fluid'));
+		echo Form::open(null, array('id' => 'form-music', 'class' => 'row'));
 
 ?>
 
-			<div class="span8">
+			<div class="col-md-8">
 				<fieldset>
-					<?= Form::control_group(
-						Form::input('name', $this->track->name, array('class' => 'input-block-level')),
-						array('name' => __('Name')),
-						Arr::get($this->errors, 'name')) ?>
+					<?= Form::input_wrap(
+						'name',
+						$this->track->name,
+						array('class' => 'input-lg'),
+						__('Name'),
+						Arr::get($this->errors, 'name')
+					) ?>
 
-					<?= Form::control_group(
-						Form::input('url', $this->track->url, array('class' => 'input-block-level', 'placeholder' => 'http://')),
-						array('url' => __('URL')),
-						Arr::get($this->errors, 'url')) ?>
+					<?= Form::input_wrap(
+						'url',
+						$this->track->url,
+						array('placeholder' => 'http://'),
+						__('URL'),
+						Arr::get($this->errors, 'url')
+					) ?>
 
-					<?= Form::control_group(
-						Form::textarea_editor('description', $this->track->description, array('class' => 'input-block-level'), true),
-						array('description' => __('Description')),
-						Arr::get($this->errors, 'description')) ?>
+					<?= Form::textarea_wrap(
+						'description',
+						$this->track->description,
+						null,
+						true,
+						__('Description'),
+						Arr::get($this->errors, 'description')
+					) ?>
 
-					<?php if ($this->track->type == Model_Music_Track::TYPE_MIX) echo Form::control_group(
-						Form::textarea('tracklist', $this->track->tracklist, array('class' => 'input-block-level'), true),
-						array('tracklist' => __('Tracklist')),
-						Arr::get($this->errors, 'tracklist')) ?>
-
+					<?php if ($this->track->type == Model_Music_Track::TYPE_MIX) echo Form::textarea_wrap(
+						'tracklist',
+						$this->track->tracklist,
+						null,
+						true,
+						__('Tracklist'),
+						Arr::get($this->errors, 'tracklist')
+					) ?>
 				</fieldset>
 
 				<fieldset class="form-actions">
@@ -81,31 +94,39 @@ class View_Music_Edit extends View_Article {
 				</fieldset>
 			</div>
 
-			<div class="span4">
+			<div class="col-md-4">
 				<fieldset>
+					<?= Form::input_wrap(
+						'cover',
+						$this->track->cover,
+						array('placeholder' => 'http://'),
+						__('Cover'),
+						Arr::get($this->errors, 'cover')
+					) ?>
 
-					<?= Form::control_group(
-						Form::input('cover', $this->track->cover, array('class' => 'input-block-level', 'placeholder' => 'http://')),
-						array('cover' => __('Cover')),
-						Arr::get($this->errors, 'cover')) ?>
-
-					<?= Form::control_group(
-						'<div class="input-append">'
-							. Form::input('size_time', $this->track->size_time, array(
-									'class'       => 'input-mini',
-									'maxlength'   => $this->track->type == Model_Music_Track::TYPE_MIX ? 8 : 6,
-									'placeholder' => $this->track->type == Model_Music_Track::TYPE_MIX ? __('hh:mm:ss') : __('mm:ss')
-								))
-							. '<span class="add-on">min</span>'
-							. '</div>',
-						array('size_time' => __('Length')),
-						Arr::get($this->errors, 'size_time')) ?>
-
+					<?= Form::input_wrap(
+						'size_time',
+						$this->track->size_time,
+						array(
+							'maxlength'   => $this->track->type == Model_Music_Track::TYPE_MIX ? 8 : 6,
+							'placeholder' => $this->track->type == Model_Music_Track::TYPE_MIX ? __('hh:mm:ss') : __('mm:ss')
+						),
+						__('Length'),
+						Arr::get($this->event_errors, 'size_time'),
+						null,
+						'min'
+					) ?>
 				</fieldset>
 
 				<fieldset id="fields-music">
-					<legend><?= __('Music') ?></legend>
-					<?= Form::checkboxes_wrap('tag', $this->tags(), $this->track->tags(), null, $this->errors, null, 'block-grid two-up') ?>
+					<?= Form::checkboxes_wrap(
+						'tag',
+						$this->tags(),
+						$this->track->tags(),
+						array('class' => 'block-grid three-up'),
+						__('Music'),
+						$this->errors
+					) ?>
 				</fieldset>
 			</div>
 
